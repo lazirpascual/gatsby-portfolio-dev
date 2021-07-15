3b9386f5bccf51bdf008b89410e5a3676f42b2df
Lazir Pascual <lazirpascual@gmail.com>
2021-07-15 14:34:48 -0400

    update personal info and projects

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,8 +10,8 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Lazir Pascual,',
+  subtitle: 'An aspiring Software Developer',
   cta: '',
 };
 
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/1HIG0Ldy3opzcXxsaQdkDGhY_YPCQ7Xkb/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -29,37 +29,46 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: 'Shopping Website',
+    info: 'Fullstack MERN application that models an e-commerce site that lets you buy computer parts. Users can add, remove, and update products from a shopping cart as well as filter products by categories, all stored in a MongoDB Database. Frontend uses React.js and Material-UI to develop a responsive design, which is supported with a backend built with Node.js and Express API.',
+    info2: 'Built with React.js, Node.js, Express API, MongoDB, REST API, and Material-UI.',
+    url: 'https://cloudcomputers.herokuapp.com/',
+    repo: 'https://github.com/lazirpascual/shopping-website', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
+    title: 'CV Builder',
     info: '',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://lazirpascual.github.io/react-cv-builder/',
+    repo: 'https://github.com/lazirpascual/react-cv-builder', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'project.jpg',
-    title: '',
+    title: 'To-Do List',
     info: '',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://lazirpascual.github.io/react-to-do-list/',
+    repo: 'https://github.com/lazirpascual/react-to-do-list', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: 'Weather App',
+    info: '',
+    info2: '',
+    url: 'https://lazirpascual.github.io/weather-app/',
+    repo: 'https://github.com/lazirpascual/weather-app', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
+  cta: 'For inquiries, email me at lazirpascual@gmail.com',
   btn: '',
-  email: '',
+  email: 'lazirpascual@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +76,23 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'instagram',
+      url: 'https://www.instagram.com/lazir.p/',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/lazir-pascual-00482a217/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/lazirpascual',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
